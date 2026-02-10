8cb1d12ef69d074a7d94e5203b3d089ca7e35f6a
Kaio Vinicius Silva Lopes <kaio.vsl11@gmail.com>
2026-02-10 07:55:02 -0400

    Atualização

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def apply_ssl_fix():
     except Exception:
         pass
 
-VERSAO = "1.1.0"
+VERSAO = "1.1.1"
 
 # URL padrão
 DEFAULT_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSfLyptmo3NFUx8dxC7k0obmQxAXPuimBLC_L30xgZOsygvqpg/viewform"
